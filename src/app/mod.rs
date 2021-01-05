a3a566ec1af53866cbcdd25f3815469a602e0ad4
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-01-05 01:41:33 +0100

    Setup tcp listener

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -18,23 +18,65 @@ pub fn start(args: Args) -> Result<(), String> {
     {
         print_err(e);
     }
-    loop {}
 
-    start_server(&config)?;
+    start_server(&config, &mut player)?;
 
     Ok(())
 }
 
-fn start_server(config: &Config) -> Result<(), String> {
-    use std::net::TcpListener;
+fn start_server(
+    config: &Config,
+    music_player: &mut MusicPlayer,
+) -> Result<(), String> {
+    use std::io::{Read, Write};
+    use std::net::{TcpListener, TcpStream};
 
-    let tcp_listener =
-        TcpListener::bind((config.host, config.port)).map_err(|e| {
-            format!(
-                "Couldn't bind to address \"{}:{}\"\n{}",
-                config.host, config.port, e
-            )
-        })?;
+    let addr = (config.host, config.port);
+    println!("Listening on {}:{}", addr.0, addr.1);
+
+    let tcp_listener = TcpListener::bind(addr).map_err(|e| {
+        format!(
+            "Couldn't bind to address \"{}:{}\"\n{}",
+            config.host, config.port, e
+        )
+    })?;
+
+    for mut stream in tcp_listener.incoming() {
+        match stream.as_mut() {
+            Ok(stream) => {
+                let mut msg = Vec::new();
+                let data = {
+                    let mut raw = [0; 512];
+                    let _ = stream.read(&mut raw);
+                    let s = String::from_utf8(raw.to_vec());
+                    match s {
+                        Ok(s) => s,
+                        Err(e) => {
+                            msg.push(format!(
+                                "Request data reading error: {}",
+                                e,
+                            ));
+                            String::new()
+                        }
+                    }
+                };
+                let now = chrono::Local::now();
+                println!(
+                    "--- INCOMING DATA {}\n{}\n{}",
+                    now.format("%H:%M:%S"),
+                    &data,
+                    msg.join("\n"),
+                );
+
+                let _ = stream.write(data.as_bytes());
+
+                stream.flush().unwrap();
+            }
+            Err(e) => {
+                eprintln!("Connection error: {}", e);
+            }
+        }
+    }
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate chrono;
 extern crate clap;
 extern crate directories;
 extern crate rand;
